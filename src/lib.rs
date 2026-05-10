878d937ddbb9ec5cacf9f3e355e785d74a694bb7
SaucePackets <jerry.daniel.kingsbury@gmail.com>
2026-05-09 19:16:41 -0500

    Implement wallet state

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 pub mod amount;
 pub mod balance;
 pub mod utxo;
+pub mod wallet;
 
 pub use amount::Amount;
 pub use balance::{BalanceSummary, calculate_balance, classify_balance};
 pub use utxo::{OutPoint, Utxo};
+pub use wallet::WalletState;
 
 pub fn dojo_ready() -> bool {
     true
