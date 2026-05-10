1f57ee576c57a79885c720cef0251b043b47967a
SaucePackets <jerry.daniel.kingsbury@gmail.com>
2026-05-09 23:29:43 -0500

    Bug fix

--- src/chain.rs
+++ src/chain.rs
@@ -2,7 +2,7 @@ use crate::utxo::Utxo;
 
 pub fn confirmations(utxo: &Utxo, tip_height: u32) -> u32 {
     match utxo.seen_at_height {
-        Some(height) => tip_height - height + 1,
-        None => 0,
+        Some(height) if height <= tip_height => tip_height - height + 1,
+        _ => 0,
     }
 }
